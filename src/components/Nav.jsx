4949c71eef2c3f11ad8832a94347eb08e655e016
Drayko <draykom@gmail.com>
2022-05-18 18:55:32 +1000

    Initial commit v2?

---
+++ src/components/Nav.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+// navigation links
+export default function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/volcano-list">Volcano List</Link>
+        </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
---
+++ src/pages/VolcanoList.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function VolcanoList() {
+  return <h2>Volcano</h2>;
+}
