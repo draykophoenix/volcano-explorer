eec8c5cd3c8e94a0c6b81b6aefe61d91191c3326
Drayko <draykom@gmail.com>
2022-05-18 18:54:58 +1000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
+import "./App.css";
+
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+
+import Home from "./pages/Home"
+import VolcanoList from "./pages/VolcanoList"
+import Register from "./pages/Register"
+import Login from "./pages/Login"
+
+export default function App() {
+  return (
+    <BrowserRouter>
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/volcano-list" element={<VolcanoList />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <Footer />
+    </div>
+    </BrowserRouter>
+  );
+}
