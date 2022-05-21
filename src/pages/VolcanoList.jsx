718b85871cfbdbaad4d3ffacc56fcfa49f4826b1
Drayko <draykom@gmail.com>
2022-05-21 14:43:39 +1000

    Moved functions around in VolcanoList

--- src/pages/VolcanoList.jsx
+++ src/pages/VolcanoList.jsx
@@ -10,49 +10,11 @@ import "ag-grid-community/dist/styles/ag-theme-balham.css";
 import { useCountries, getVolcanoDataByQuery } from "../api";
 
 export default function VolcanoList() {
-  const navigate = useNavigate();
-  const {rowData, volcanoError} = {rowData: [], volcanoError: null}; /*getVolcanoDataByQuery("Japan");*/ 
-  const {loading, countries, countryError} = useCountries();
-
-  //const [selectedCountry, setSelectedCountry] = useState('');
-
-  const columns = [
-    { headerName: "Name", field: "name", sortable: true },
-    { headerName: "Region", field: "region" },
-    { headerName: "Subregion", field: "subregion", sortable: true },
-  ];
-
-
   return (
-  <div className= "container">
+    <div className="container">
       <h1>Book Catalouge</h1>
-      <p><Badge color="success">{rowData.length}</Badge> Books published in 2000 in the Drama catageory</p>
 
-      {loading ? (
-        <p>Loading... </p>
-      ) : (
-          <Input type="select">
-          {countries.map((country) => (
-            <option>{country}</option>
-          ))}
-          </Input>
-      )}
-
-      <div 
-        className="ag-theme-balham"
-        style={{
-          height: "300px",
-          width: "600px"
-        }}
-      >
-        <AgGridReact
-          columnDefs={columns}
-          rowData={rowData}
-          pagination= {true} 
-          paginationPageSize= {7}
-          onRowClicked={(row) => navigate(`./volcano?title=${row.data.name}`)}
-        />
-      </div>
+      <VolcanoTable/>
 
       <Button
         color="info"
@@ -62,7 +24,77 @@ export default function VolcanoList() {
         target="_blank"
         >
           Go to Open Library API
-        </Button>
+      </Button>
   </div>
   );
 }
+
+  function VolcanoTable () {
+  // Volcano data for rows in the volcano table
+  const {rowData, volcanoError} = getVolcanoDataByQuery("Japan");
+  // Countries for the input box 
+  const {loading, countries, countryError} = useCountries();
+  // Allows table to navigate with React Router
+  const navigate = useNavigate();
+
+  const [selectedCountry, setSelectedCountry] = useState('');
+
+  const columns = [
+    { headerName: "Name", field: "name", sortable: true },
+    { headerName: "Region", field: "region" },
+    { headerName: "Subregion", field: "subregion", sortable: true },
+  ];
+
+  return (
+    <div className= "container">
+    {loading ? (
+      <p>Loading... </p>
+    ) : (
+      <form 
+        onSubmit={(event) => {
+          event.preventDefault();
+          console.log(event.target.elements.country.value);
+        }}
+      >
+        <label htmlFor="country">Country:</label>
+        <input id="country" name="country" type="text" />
+        <button type="submit">Submit</button>
+      </form>
+    )}
+
+    <p>
+      <Badge color="success">{rowData.length}</Badge> Books published in 2000 in the Drama catageory
+    </p>
+
+    <div 
+      className="ag-theme-balham"
+      style={{
+        height: "300px",
+        width: "600px"
+      }}
+    >
+      <AgGridReact
+        columnDefs={columns}
+        rowData={rowData}
+        pagination= {true} 
+        paginationPageSize= {7}
+        onRowClicked={(row) => navigate(`./volcano?title=${row.data.name}`)}
+      />
+    </div>
+  </div>
+  )
+}
+
+    {/* {loading ? (
+      <p>Loading... </p>
+    ) : (
+        <Input 
+          type="select"
+          value={selectedCountry}
+          onChange={(entry) => setSelectedCountry(entry.target.value)}
+        >
+        {countries.map((country) => (
+          <option>{country}</option>
+        ))}
+        </Input>
+    )} */}
